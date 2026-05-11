459e1059f2fc3a25f354b6f051cfead675c4d88c
muraina david <muraina david>
2026-05-11 23:20:43 +0100

    i'm still working on creating the database for the cansat

--- client.py
+++ client.py
@@ -9,17 +9,18 @@ import asyncpg
 import struct
 import asyncio
 from websockets.asyncio.client import connect
-async def get_data():
+async def data_receiver():
     async with connect("ws://localhost:4443") as websocket:
         #message = await websocket.recv()
        # await websocket.send(json.dumps([interval, max_altitude, speed]))
         async for message in websocket:
-            print(struct.unpack("< I i i i h H B H h h h h h h H H ", message), 34)
-            await asyncio.sleep(0.02)
+            decoded = struct.unpack("< I i i i h H B H h h h h h h H H ", message)
+            print(decoded)
+
 
 async def main():
     conn = await asyncpg.connect('postgresql://postgres@localhost/test')
-    await conn.execute(''' CREATE TABLE IF NOT EXISTS TELEMETRY(PRESSURE INT NOT NULL,HUMIDITY INT NOT NULL,TEMPERATURE INT NOT NULL
+    await conn.execute(''' CREATE TABLE IF NOT EXISTS TELEMETRY(TIMESTAMP INT NOT NULL,HUMIDITY INT NOT NULL, GPS_LATTITUDE INT NOT NULL, GPS_LONGITUDE INT NOT NULL, ALTITUDE INT NOT NULL, TEMPERATURE INT NOT NULL,PRESSURE INT NOT NULL, HUMIDITY INT NOT NULL, LUMINOUS_INTENSITY INT NOT NULL,ACCELERATION_X INT NOT NULL, ACCELERATION_Y INT NOT NULL, ACCELERATION_Z INT NOT NULL,GYRO_X INT NOT NULL, GYRO_Y INT NOT NULL, GYRO_Z INT NOT NULL, VOLTAGE INT NOT NULL, CURRENT INT NOT NULL''')
 try:
     asyncio.run(get_data())
 except KeyboardInterrupt:
