cec5eb6a025ec0094e1787fd5563f4c20fb80c54
muraina david <muraina david>
2026-05-17 19:53:57 +0100

    i have created data  receiver and data storer functions to get data from server and seve it to a database asynchronously

--- client.py
+++ client.py
@@ -7,19 +7,29 @@ speed  = float(input("specify ascent speed"))
 """
 import asyncpg
 import struct
-import asyncio
+from asyncio import Queue
 from websockets.asyncio.client import connect
+
+Decode_queue = Queue()
 async def data_receiver():
-    async with connect("ws://localhost:4443") as websocket:
+    try async with connect("ws://localhost:4443") as websocket:
         #message = await websocket.recv()
        # await websocket.send(json.dumps([interval, max_altitude, speed]))
         async for message in websocket:
             decoded = struct.unpack("< I i i i h H B H h h h h h h H H ", message)
             print(decoded)
+        Decode_queue.put_nowait(decoded)
+    except asyncio.CancelledError:
+        print("Data receiver stopped")
+    except Exception as e:
+        print(f"This exception occured: {e}")
 
 
+async def database_writer(conn):
+    print("starting database saver")
+    insert_query =
 async def main():
-    conn = await asyncpg.connect('postgresql://postgres@localhost/test')
+    conn = await asyncpg.connect('postgresql://postgres@localhost/Cansat')
     await conn.execute(''' CREATE TABLE IF NOT EXISTS TELEMETRY(TIMESTAMP INT NOT NULL,HUMIDITY INT NOT NULL, GPS_LATTITUDE INT NOT NULL, GPS_LONGITUDE INT NOT NULL, ALTITUDE INT NOT NULL, TEMPERATURE INT NOT NULL,PRESSURE INT NOT NULL, HUMIDITY INT NOT NULL, LUMINOUS_INTENSITY INT NOT NULL,ACCELERATION_X INT NOT NULL, ACCELERATION_Y INT NOT NULL, ACCELERATION_Z INT NOT NULL,GYRO_X INT NOT NULL, GYRO_Y INT NOT NULL, GYRO_Z INT NOT NULL, VOLTAGE INT NOT NULL, CURRENT INT NOT NULL''')
 try:
     asyncio.run(get_data())
